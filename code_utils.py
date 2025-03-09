87896bafed76e6a531b32561c28a30468ac6e5f2
anastasia <anastasia.lurye@lean-ai-tech.com>
2025-03-09 17:28:26 +0200

    added CyclicRotation

--- code_utils.py
+++ code_utils.py
@@ -21,3 +21,23 @@ def find_max_gap(bin_str: str)->int:
         else: 
             current_gap += 1
     return max_gap
+
+'''
+CyclicRotation
+Rotate an array to the right by a given number of steps.
+'''
+def rotate_array(A, K):
+    '''Rotates an array to the right by a given number of steps'''
+    if not A or K == 0:
+        return A
+    l=len(A)
+    if l==1:
+        return A
+    k = find_k(l=l, K=K)
+    if k==0:
+        return A
+    return A[-k:]+A[:-k]
+
+def find_k(l:int, K:int)->int:
+    '''Finds the number of steps to rotate an array'''
+    return K%l
